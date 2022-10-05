5e4ae99d75d1baefcb91e2543735c430821fa917
Pablo20053 <114589467+Pablo20053@users.noreply.github.com>
2022-10-05 08:01:02 -0500

    preuba

---
+++ fecha.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Word</title>
+    <link rel="stylesheet" href="estilos.css">
+
+</head>
+<body>
+     <?php
+        echo"<H1>";
+        echo"Fecha:28/09/2022";
+        echo"</H1>"
+
+     ?>
+
+</body>
+</html>
